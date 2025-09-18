bef9db29bdf49548accae2585289ee97d48fb2b4
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-17 19:30:15 -0700

    Spring Boot project  week 1

--- src/main/java/com/gulnara/internship/Hello.java
+++ src/main/java/com/gulnara/internship/Hello.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Hello {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, Welcome to Spring bloom!";
     }
 }
