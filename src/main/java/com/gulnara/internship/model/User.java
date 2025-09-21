1c5dde7dd3723594d28c98b9a89a2fe7a650fb7a
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-09-20 20:06:26 -0700

    feat: add User entity

---
+++ src/main/java/com/gulnara/internship/model/User.java
@@ -0,0 +1,40 @@
+package com.gulnara.internship.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    public User() {}
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
