331f5c0e4aa0563e0cb31279b5a28e6fa4c4fb45
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-25 01:38:08 -0700

    test: fix InternProjectApplicationTests

--- src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
+++ src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.main.web-application-type=servlet")
+@SpringBootTest //(properties = "spring.main.web-application-type=servlet")
 @ActiveProfiles("test")
 class InternProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// Проверяем только, что контекст поднимается
 	}
 }
