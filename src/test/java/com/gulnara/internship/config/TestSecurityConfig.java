cf1d1afc6088deddbaddd6cc9effaa92a3504f47
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-25 00:46:45 -0700

    fix: add test application properties for CI environment

--- src/test/java/com/gulnara/internship/config/TestSecurityConfig.java
+++ src/test/java/com/gulnara/internship/config/TestSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 
 @TestConfiguration
 public class TestSecurityConfig {
