c929d02667d957135e1584c1d5a15d5fab686e2e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-10-26 16:20:12 -0700

    fix: stabilize Spring context test for CI

--- src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
+++ src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.name=application-test")
 @ActiveProfiles("test")
+
 class InternProjectApplicationTests {
 
 	@Test
