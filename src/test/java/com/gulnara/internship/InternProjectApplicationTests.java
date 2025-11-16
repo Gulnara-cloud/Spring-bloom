4ed4952004c6c0e777a30ac01a7c3acfbe061e4e
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-16 15:07:50 -0800

    fix: refactor application test class

--- src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
+++ src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 @ActiveProfiles("test")
 class InternProjectApplicationTests {
 
