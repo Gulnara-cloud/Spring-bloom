ed464f6d119316c10263c5593e849e892d9c1fc5
Gulnara Nuraly <gulnara.nuraly@icloud.com>
2025-11-22 19:01:47 -0800

    fix: correct @ActiveProfiles order for test context

--- src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
+++ src/test/java/com/gulnara/internship/InternProjectApplicationTests.java
@@ -2,11 +2,10 @@ package com.gulnara.internship;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
 class InternProjectApplicationTests {
 	@Test
 	void contextLoads() {
